f252f668c0915cd1a327f205c691210c1e14384a
Tim Bruecker <brooker_113@hotmail.com>
2018-03-07 10:56:21 -0800

    Fully Done, only need documentation

--- client.c
+++ client.c
@@ -1,5 +1,12 @@
 #include "commonCore.h"
 
+void* checkInput();
+void* checkForKill();
+
+int qid;
+int pid;
+struct my_message msgSend;
+
 int main (int argc , char *argv[])
 {
 	if (argc != 3)
@@ -9,22 +16,28 @@ int main (int argc , char *argv[])
 		return 0;
 	}
 
+	pthread_t qThread;
+	pthread_t dieThread;
+
 	// Copy argument to variable
 	char FileName[strlen(argv[1])];
 	strcpy(FileName, argv[1]);
 	char* priority = argv[2];
 
 	// Get pid to use as indentifier
-	int pid = getpid();
+	pid = getpid();
 
 	// https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c
 	char strPid[(int)((ceil(log10(pid))+1)*sizeof(char))];
 	sprintf(strPid, "%d", pid);
 	
 
-	struct my_message msgSend;
+	
 	struct my_message recvBuf;
-	int qid = open_queue(6969);
+	qid = open_queue(MQKEY);
+
+	pthread_create(&qThread, NULL, checkInput, NULL);
+	pthread_create(&dieThread, NULL, checkForKill, NULL);
 
 
 	memset(msgSend.mtext, '\0', MTEXTLEN);
@@ -47,9 +60,31 @@ int main (int argc , char *argv[])
 	}
 	while(recvBuf.mtext[0] != 4);
 
-	
+	fflush(stdout);
 
- 	
+	return 0;
+}
 
+void* checkInput()
+{
+	while(1)
+	{
+		char temp = getchar();
+		if(temp == 'q')
+		{
+			msgSend.mtext[0] = '\x03';
+			msgSend.mtype = CONNECTMSGID;
+			send_message(qid, &msgSend);
+		}
+	}
+	return 0;
+}
+
+void* checkForKill()
+{
+	struct my_message recvKillBuf;
+	read_message(qid, DIEMSGID, &recvKillBuf);
+	printf("%s\n", "Terminated by q on a client");
+	exit(0);
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,86 +1,97 @@
 #include "commonCore.h"
 
-int getEmptyClientID(struct connected_client clientList[]);
-void initClientList(struct connected_client clientList[]);
-void deleteClient(struct connected_client clientList[], int id);
 
+void getSem(int sid);
+void releaseSem(int sid);
+int initsem (key_t key);
+void quitEverything(int numClients, int qid, int semid);
+
+extern int errno;
 
 int main (int argc , char *argv[])
 {
 	struct my_message rcvConnects;
-	struct my_message sendBuf;
-	int qid = open_queue(6969);
-	int running = 1;
+	int qid = open_queue(MQKEY);
 	int newConnection;
-	int currClients = 0;
+	int semKey = 100;
+	int semid;
+	int numClients;
 
-	struct connected_client clients[MAXCLIENTS];
-	initClientList(clients);
+	semid = initsem ((key_t)semKey);
+	releaseSem(semid);
+
 
 	// Polling Loop
-	while(running)
+	while(1)
 	{
-		newConnection = read_message_noblock(qid, CONNECTMSGID, &rcvConnects);
+		newConnection = read_message(qid, CONNECTMSGID, &rcvConnects);
 
-		if(newConnection != -1 && currClients < MAXCLIENTS)
+		if(rcvConnects.mtext[0] == '\x03')
 		{
-
-			char tempFileName[MTEXTLEN];
-			int tempPid;
-			int tempPriority;
-			int emptyClient = getEmptyClientID(clients);
-			sscanf(rcvConnects.mtext, "%s %d %d", tempFileName, &tempPid, &tempPriority);
-			printf("New client wants: %s\nAt pid: %d\nWith priority: %d\n\n", tempFileName, tempPid, tempPriority);
-
-			clients[emptyClient].file = fopen(tempFileName, "r");
-			clients[emptyClient].pid = tempPid;
-			clients[emptyClient].priority = tempPriority;
-
-			if(clients[emptyClient].file == NULL)
-			{
-				// Send error string to client
-				strcpy(sendBuf.mtext, "\x04 Error: Invalid filename");
-				sendBuf.mtype = clients[emptyClient].pid;
-				send_message(qid, &sendBuf);
-				deleteClient(clients, emptyClient);
-				perror("Problem opening file");
-			}
-			else
-			{
-				currClients++;
-			}
-
-			
-			newConnection = -1;
+			quitEverything(numClients, qid, semid);
 		}
-		else if (currClients >= MAXCLIENTS)
+		else if(newConnection != -1)
 		{
-			printf("%s\n", "Error: Max Clients reached, wait for a transmission to finish");
-		}
+			FILE *fileHandle;
+			char fileName[MTEXTLEN];
+			int pid;
+			int priority;
+			sscanf(rcvConnects.mtext, "%s %d %d", fileName, &pid,  &priority);
+			printf("New client wants: %s\nAt pid: %d\nWith priority: %d\n\n", fileName, pid, priority);
 
-		for(int i = 0; i < MAXCLIENTS; i++)
-		{
-			if(clients[i].pid != 0)
+
+
+			if(fork())
 			{
-				for(int j = 0; j < clients[i].priority; j++)
+				struct my_message sendBuf;
+				int fileSent = 0;
+				// Try to Open the file
+				fileHandle = fopen(fileName, "r");
+				if(fileHandle == NULL)
 				{
-					//printf("%s %d\n", "Wrote to", clients[i].pid);
-					fgets(sendBuf.mtext, MTEXTLEN, clients[i].file);
-					sendBuf.mtype = clients[i].pid;
-
+					// Send error string to client
+					strcpy(sendBuf.mtext, "\x04 Error: Invalid filename");
+					sendBuf.mtype = pid;
 					send_message(qid, &sendBuf);
-					
-					if(feof(clients[i].file))
-					{
-						strcpy(sendBuf.mtext, "\x04");
-						send_message(qid, &sendBuf);
-						deleteClient(clients, i);
-						currClients--;
-						break;
-					}
-					
+					perror("Problem opening file");
 				}
+				else
+				{
+					// Send Loop
+					while(!fileSent)
+					{
+						// Start Writing * Priority
+						getSem(semid);
+						printf("%s%d%s%d\n", "Got sem ID: ", pid, " priority: ", priority);
+						for(int j = 0; j < priority; j++)
+						{
+							printf("Writing to: %d\n", pid);
+							fgets(sendBuf.mtext, MTEXTLEN, fileHandle);
+							sendBuf.mtype = pid;
+
+							send_message(qid, &sendBuf);
+							
+							if(feof(fileHandle))
+							{
+								strcpy(sendBuf.mtext, "\x04");
+								send_message(qid, &sendBuf);
+								fileSent = 1;
+								break;
+							}
+						}
+
+						releaseSem(semid);
+						printf("%s%d\n\n", "Released sem ID: ", pid);
+						sched_yield();
+					}
+					exit(0);
+				}
+				
 			}
+
+			// Reset Connection;
+			numClients++;
+			newConnection = -1;
 		}
 		
 	}
@@ -89,33 +100,49 @@ int main (int argc , char *argv[])
 }
 
 
-int getEmptyClientID(struct connected_client clientList[])
-{
-	for(int i = 0; i < MAXCLIENTS; i++)
-	{
-		if(clientList[i].pid == 0)
-		{
-			return i;
-		}
-	}
 
-	return -1;
+int initsem (key_t key)
+{
+  int sid;
+  sid = semget( (key_t)key, 1, IPC_CREAT | 0660 );
+  if(sid == -1)
+  {
+  	perror("semget error:\n");
+  }
+  return sid;
 }
 
-void initClientList(struct connected_client clientList[])
+
+void getSem(int sid)     /* acquire semophore */
 {
-	for(int i = 0; i < MAXCLIENTS; i++)
-	{
-		clientList[i].file = NULL;
-		clientList[i].pid = 0;
-		clientList[i].priority = 1;
-	}
+    struct sembuf sembuf_ptr = {0, -1, 0};
+
+    if ((semop(sid, &sembuf_ptr, 1)) == -1)
+    {
+    	perror("semop error\n");
+    }
 }
 
-void deleteClient(struct connected_client clientList[], int id)
+void releaseSem(int sid)     /* release semaphore */
 {
-	fclose(clientList[id].file);
-	clientList[id].file = NULL;
-	clientList[id].pid = 0;
-	clientList[id].priority = 0;
+	struct sembuf sembuf_ptr = {0, 1, 0};
+
+    if ((semop(sid, &sembuf_ptr, 1)) == -1)
+    {
+    	perror("semop error\n");
+    }
+}
+
+void quitEverything(int numClients, int qid, int semid)
+{
+	for(int i = 0; i < numClients; i++)
+			{
+				struct my_message killStruct;
+				killStruct.mtype = DIEMSGID;
+				send_message(qid, &killStruct);
+			}
+
+			msgctl (qid, IPC_RMID, 0);
+			semctl(semid, 0, IPC_RMID, 0);
+			kill(0, SIGKILL);
 }
